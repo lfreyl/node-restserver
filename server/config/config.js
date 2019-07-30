323ac455ed9558c41ede83bc1907aca62d800cb6
Cristian <cristian930714@hotmail.com>
2019-07-29 20:07:58 -0500

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://frey:KaSes6a3Jy1I3wkd@cluster0-uidno.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 };
 process.env.URLDB = urlDB;
